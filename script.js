4cf150972c3a947fd13e29f95bebfeb25673c797
andirese <andirenaldisetiawan@gmail.com>
2025-06-30 05:37:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,32 +21,21 @@ window.addEventListener('hashchange', () => {
       link.classList.remove('active');
     }
   });
-  <script>
-  // Fungsi untuk ambil nama dari URL
-  function getQueryParam(param) {
-    const params = new URLSearchParams(window.location.search);
-    return params.get(param);
-  }
-
-  const nama = getQueryParam('to');
-  const namaTamu = document.getElementById('namaTamu');
-
-  if (nama) {
-    namaTamu.innerText = decodeURIComponent(nama.replace(/\+/g, ' '));
-  } else {
-    namaTamu.innerText = 'Tamu Undangan';
-  }
-
-  // Fungsi buka undangan
-  const bukaUndangan = document.getElementById('bukaUndangan');
-  const opening = document.getElementById('opening');
-  const music = document.getElementById('music');
-
-  bukaUndangan.addEventListener('click', () => {
-    opening.style.display = 'none';
-    music.play();
-    window.location.hash = '#home';
-  });
-</script>
-
 });
+
+// ===========================
+// Ambil nama tamu dari URL
+// ===========================
+function getQueryParam(param) {
+  const params = new URLSearchParams(window.location.search);
+  return params.get(param);
+}
+
+const nama = getQueryParam('to');
+const namaTamu = document.getElementById('namaTamu');
+
+if (nama) {
+  namaTamu.innerText = decodeURIComponent(nama.replace(/\+/g, ' '));
+} else {
+  namaTamu.innerText = 'Tamu Undangan';
+}
